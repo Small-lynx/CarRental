31433a601c2bff3e981f2126310e960115802941
Small-lynx <lynxrufus2k4@gmail.com>
2025-03-30 14:35:06 +0300

    Revert "Удаление Model"
    This reverts commit 92a7ab03f0c37068c0cf6cdc225ce53c2838e462.

---
+++ CarRental.BL.Contract.Model/CarRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRental.BL.Contract.Model;
+/// <summary>
+/// Данные машины
+/// </summary>
+/// <param name="CarMake">Марка машины</param>
+/// <param name="StateNumber">Гос номер</param>
+/// <param name="Mileage">Пробег</param>
+/// <param name="AvgFuelConsumption">Средний расход топлива за час</param>
+/// <param name="FuelVolume">Текущий объем топлива</param>
+/// <param name="RentalCost">Стоимость аренды (за минуту)</param>
+public record CarRequest(string CarMake, string StateNumber, decimal Mileage, decimal AvgFuelConsumption, decimal FuelVolume, decimal RentalCost) { }
