ccd17ba0b298cf4634cff037cd06daf821bece08
Small-lynx <lynxrufus2k4@gmail.com>
2025-05-01 18:04:22 +0300

    Добавления логирования веб части

--- CarRental.Beb/Program.cs
+++ CarRental.Beb/Program.cs
@@ -3,6 +3,8 @@ using CarRental.BL.Contract;
 using CarRental.BL.Contract.Model;
 using CarRental.Storage.Contract;
 using CarRental.Storage.SQL;
+using Serilog.Extensions.Logging;
+using Serilog;
 
 namespace CarRental.Beb
 {
@@ -11,12 +13,20 @@ namespace CarRental.Beb
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CarRental.Storage.SQL.CarRentalContext;Integrated Security=True;";
+            var logger = new LoggerConfiguration()
+            .MinimumLevel.Debug() // Уровень логирования
+            .WriteTo.Seq("http://localhost:5341", apiKey: "false") // URL вашего Seq сервера
+            .CreateLogger();
+            var micLogger = new SerilogLoggerFactory(logger)
+                .CreateLogger(nameof(CarRental.Beb.Program));
+
 
             builder.Services.AddScoped<IStorage<Car>>(provider =>
-        new CarRentalStorage(connectionString, null));
+        new CarRentalStorage(connectionString, micLogger));
             builder.Services.AddScoped<ICarManeger>(provider =>
-        new CarManeger(provider.GetRequiredService<IStorage<Car>>(), null));
+        new CarManeger(provider.GetRequiredService<IStorage<Car>>(), micLogger));
             builder.Services.AddControllersWithViews();
             
             var app = builder.Build();
